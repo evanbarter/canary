2da2c4a187003b0b8a5316227f06b5b3d1c8d6f7
Evan Barter <evan@evanbarter.me>
2020-06-13 23:20:45 -0700

    Add animation to image posts in list

--- resources/views/components/post/list/image.blade.php
+++ resources/views/components/post/list/image.blade.php
@@ -1,3 +1,3 @@
-<div x-on:click="view({{ $post->id }}, $dispatch)" class="h-64 rounded-lg shadow-xl text-gray-700 bg-gray-50 hover:bg-gray-100 hover:text-gray-900 dark-mode:bg-gray-800 dark-mode:hover:bg-gray-900 dark-mode:text-gray-200 dark-mode:hover:text-gray-400 cursor-pointer">
-    <img class="h-64 w-full rounded-lg object-cover" src="{{ $post->postable->getFirstMediaUrl('images') }}">
+<div x-on:click="view({{ $post->id }})" class="h-64 rounded-lg shadow-xl text-gray-700 bg-gray-50 hover:bg-gray-100 hover:text-gray-900 dark-mode:bg-gray-800 dark-mode:hover:bg-gray-900 dark-mode:text-gray-200 dark-mode:hover:text-gray-400 cursor-pointer">
+    <img class="h-64 transition duration-150 ease-in-out transform hover:scale-105 w-full rounded-lg object-cover" src="{{ $post->postable->getFirstMediaUrl('images') }}">
 </div>
