98b067891568f1d7f23ab0b0378b76610128a0a1
Evan Barter <evan@evanbarter.me>
2020-06-20 15:55:27 -0700

    Improve visibility selector display on mobile

--- resources/views/components/post/visibility.blade.php
+++ resources/views/components/post/visibility.blade.php
@@ -1,7 +1,7 @@
 <label for="visibility" class="block text-sm font-medium text-gray-700 leading-5 dark-mode:text-white">
     {{ __('Visibility') }}
 </label>
-<div id="visibility" x-data="{ selected: {{ $visibility }} }" class="grid grid-cols-3 gap-1 sm:gap-2">
+<div id="visibility" x-data="{ selected: {{ $visibility }} }" class="grid grid-cols-1 sm:grid-cols-3 gap-1 sm:gap-2">
     <label x-bind:class="{ 'bg-indigo-100 border-indigo-400': selected === 1 }" @click="selected = 1" for="visibility-1" class="w-full flex p-2 shadow-sm border-2 rounded text-sm">
         <div class="mr-2"><input wire:model="visibility" id="visibility-1" class="form-radio text-indigo-600" type="radio" name="-1" value="1" /></div>
         <div class="flex flex-col">
