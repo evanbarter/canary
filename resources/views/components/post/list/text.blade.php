a217f889139c86d93331e05961c7b53b0c2ea1be
Evan Barter <evan@evanbarter.me>
2020-07-16 10:45:22 -0700

    Style tweaks for text posts

--- resources/views/components/post/list/text.blade.php
+++ resources/views/components/post/list/text.blade.php
@@ -1,5 +1,5 @@
 <div x-on:click="view('{{ $post->slug }}')" class="flex flex-col p-4 h-64 rounded-lg shadow-lg text-gray-600 bg-white hover:bg-gray-50 hover:text-gray-900 dark-mode:bg-gray-800 dark-mode:hover:bg-gray-900 dark-mode:text-gray-200 dark-mode:hover:text-gray-400 cursor-pointer">
-    <h2 class="h-full w-full font-extrabold italic text-2xl leading-tight break-words">{{ $post->postable->title }}</h2>
+    <h2 class="h-full w-full font-semibold italic text-2xl leading-tight break-words">{{ $post->postable->title }}</h2>
     <div class="flex flex-row-reverse">
         <span class="flex-1 text-right text-xs" title="{{ $post->created_at }}">{{ $post->created_at->diffForHumans() }}</span>
         @if ($post->syndicated)
