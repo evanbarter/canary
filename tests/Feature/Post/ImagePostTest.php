15e284b19d6850643a2ae0228ed489c530899313
Evan Barter <evan@evanbarter.me>
2020-07-21 14:58:18 -0700

    Improved test coverage for Image posts

--- tests/Feature/Post/ImagePostTest.php
+++ tests/Feature/Post/ImagePostTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Post;
 
 use App\User;
 use App\Post;
+use App\Image as ImagePost;
 use App\Http\Livewire\Post\Editor\Image;
 use Tests\TestCase;
 use Livewire\Livewire;
@@ -42,5 +43,104 @@ class ImagePostTest extends TestCase
             ->call('save');
 
         $this->assertTrue(Post::all()->count() === 1);
+    }
+
+    /** @test */
+    public function can_create_individual_image_posts()
+    {
+        $this->actingAs(factory(User::class)->create());
+        Storage::fake();
+
+        $images = [
+            UploadedFile::fake()->image('test1.jpg'),
+            UploadedFile::fake()->image('test2.jpg'),
+        ];
+
+        Livewire::test(Image::class)
+            ->set('images', $images)
+            ->set('layout', 'individual')
+            ->call('save');
+
+        $this->assertTrue(Post::all()->count() === 2);
+    }
+
+    /** @test */
+    public function title_is_optional()
+    {
+        $this->actingAs(factory(User::class)->create());
+        Storage::fake();
+
+        Livewire::test(Image::class)
+            ->set('images', UploadedFile::fake()->image('test1.jpg'))
+            ->set('titles', [])
+            ->call('save');
+
+        Livewire::test(Image::class)
+            ->set('images', UploadedFile::fake()->image('test2.jpg'))
+            ->set('titles', ['Title'])
+            ->call('save');
+
+        $this->assertTrue(ImagePost::find(1)->title[0] === '');
+        $this->assertTrue(ImagePost::find(2)->title[0] === 'Title');
+    }
+
+    /** @test */
+    public function description_is_optional()
+    {
+        $this->actingAs(factory(User::class)->create());
+        Storage::fake();
+
+        Livewire::test(Image::class)
+            ->set('images', UploadedFile::fake()->image('test1.jpg'))
+            ->set('descriptions', [])
+            ->call('save');
+
+        Livewire::test(Image::class)
+            ->set('images', UploadedFile::fake()->image('test2.jpg'))
+            ->set('descriptions', ['Desc'])
+            ->call('save');
+
+        $this->assertTrue(ImagePost::find(1)->description[0] === '');
+        $this->assertTrue(ImagePost::find(2)->description[0] === 'Desc');
+    }
+
+    /** @test */
+    public function image_can_be_removed()
+    {
+        $this->actingAs(factory(User::class)->create());
+        Storage::fake();
+
+        $images = [
+            UploadedFile::fake()->image('test1.jpg'),
+            UploadedFile::fake()->image('test2.jpg'),
+        ];
+
+        $component = Livewire::test(Image::class)
+            ->set('images', $images);
+
+        $component->call('remove', 1);
+        $files = $component->viewData('images');
+
+        $this->assertCount(1, $files);
+        $this->assertTrue($files[0]->getClientOriginalName() === 'test1.jpg');
+    }
+
+    /** @test */
+    public function emits_events()
+    {
+        $this->actingAs(factory(User::class)->create());
+        Storage::fake();
+
+        $images = [
+            UploadedFile::fake()->image('test1.jpg'),
+        ];
+
+        $component = Livewire::test(Image::class);
+        $component->set('images', $images)
+            ->call('save')
+            ->assertEmitted('postEditorSaved');
+
+        $component->call('startEditing', 1)
+            ->assertEmitted('postEditorReady');
     }
 }
