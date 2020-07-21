e62eb172ad26d875ae6b4246da112be62bc57465
Evan Barter <evan@evanbarter.me>
2020-07-21 15:04:52 -0700

    Clean up some boilerplate templates, remove unneeded functionality

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,6 +1,5 @@
 <div>
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
-
         <h2 class="mt-6 text-3xl font-extrabold text-center text-gray-900 leading-9">
             Sign in to your account
         </h2>
