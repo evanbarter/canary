e399cfb9ad20df6d0c5ea3835748528bc79a9ee4
Evan Barter <evan@evanbarter.me>
2020-07-21 16:21:14 -0700

    WIP tests for Peers

---
+++ tests/Feature/PeerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use App\Peer;
+use App\Http\Livewire\Settings;
+use Tests\TestCase;
+use Livewire\Livewire;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PeerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function url_is_required()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        Livewire::test(Settings::class)
+            ->set('peerAddURL', '')
+            ->call('addPeer')
+            ->assertHasErrors(['peerAddURL' => 'required']);
+    }
+
+    /** @test */
+    public function url_is_valid()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        Livewire::test(Settings::class)
+            ->set('peerAddURL', 'foo')
+            ->call('addPeer')
+            ->assertHasErrors(['peerAddURL' => 'url']);
+    }
+
+    /** @test */
+    public function can_create_peer()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        // Disable events as an observer will fire a HTTP request on the
+        // created event.
+        Peer::unsetEventDispatcher();
+
+        Livewire::test(Settings::class)
+            ->set('peerAddURL', 'http://example.com')
+            ->call('addPeer');
+
+        $this->assertCount(1, Peer::all());
+    }
+}
