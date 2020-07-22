df498e87c638d98c6cb4703b58d9ba2cf763df7e
Evan Barter <evan@evanbarter.me>
2020-07-22 14:55:49 -0700

    Use Auth facade in PeerRequest

--- app/Http/Requests/PeerRequest.php
+++ app/Http/Requests/PeerRequest.php
@@ -5,13 +5,14 @@ namespace App\Http\Requests;
 use App\Peer;
 use Illuminate\Http\Request;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Support\Facades\Auth;
 
 class PeerRequest extends Request
 {
     public function peer()
     {
-        foreach (auth()->user()->currentAccessToken()->abilities as $token_ability) {
-            list($type, $id, $ability) = explode(':', $token_ability);
+        foreach (Auth::user()->currentAccessToken()->abilities as $token_ability) {
+            list($type, $id, ) = explode(':', $token_ability);
             if ($type === 'peer') {
                 return Peer::findOrFail($id);
             }
