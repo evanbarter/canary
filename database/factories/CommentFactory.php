20e1347791802fd379c13b68bbda41c23efdd18b
Evan Barter <evan@evanbarter.me>
2020-07-23 10:44:25 -0700

    Additional syndication tests

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Comment;
+use Faker\Generator as Faker;
+
+$factory->define(Comment::class, function (Faker $faker) {
+    return [
+        'uuid' => $faker->uuid,
+        'comment' => $faker->text(),
+    ];
+});
--- tests/Feature/SyndicationTest.php
+++ tests/Feature/SyndicationTest.php
@@ -6,7 +6,9 @@ use App\User;
 use App\Peer;
 use App\Post;
 use App\Text;
+use App\Comment;
 use App\Http\Livewire\Post\ListPosts;
+use App\Http\Livewire\Post\ViewPost;
 use Tests\TestCase;
 use Livewire\Livewire;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -21,6 +23,12 @@ class SyndicationTest extends TestCase
     /** @var \App\Peer */
     protected $peer = null;
 
+    /** @var \App\Post */
+    protected $post = null;
+
+    /** @var \App\Comment */
+    protected $comment = null;
+
     /** @var string */
     protected $token = '';
 
@@ -31,42 +39,45 @@ class SyndicationTest extends TestCase
         $this->user = factory(User::class)->create();
         $this->peer = factory(Peer::class)->create(['verified_at' => now()]);
         $this->token = $this->user->createToken(sprintf('%s token for %s', 'Peer', $this->peer->url), [sprintf('peer:%d:%s', $this->peer->id, 'peer')])->plainTextToken;
+
+        $post = new Post(factory(Post::class)->raw());
+        $post
+            ->postable()->associate(factory(Text::class)->create())
+            ->sourceable()->associate($this->peer);
+        $this->post = $post;
+
+        $comment = new Comment(factory(Comment::class)->raw());
+        $comment
+            ->commentable()->associate($post)
+            ->sourceable()->associate(factory(User::class)->create());
+        $this->comment = $comment;
     }
 
     /** @test */
-    public function will_reject_unverified_peer_posts()
+    public function will_reject_unverified_peers()
     {
         $this->peer->update(['verified_at' => null]);
-
-        $text = factory(Text::class)->create();
-        $post = new Post(factory(Post::class)->raw());
-        $post = $post->postable()->associate($text)
-            ->sourceable()->associate($this->peer)
-            ->prepareForSyndication('saved');
 
         $response = $this->withHeaders([
             'Authorization' => 'Bearer ' . $this->token,
         ])->json('POST', route('peers.syndicate.post'), [
-            'post' => $post,
+            'post' => $this->post->prepareForSyndication('saved'),
             'event' => 'saved',
         ]);
 
         $response->assertStatus(403);
+
+        $this->actingAs($this->user);
+        $this->assertCount(0, Post::syndicated()->get());
     }
 
     /** @test */
     public function will_accept_peer_posts()
     {
-        $text = factory(Text::class)->create();
-        $post = new Post(factory(Post::class)->raw());
-        $post = $post->postable()->associate($text)
-            ->sourceable()->associate($this->peer)
-            ->prepareForSyndication('saved');
-
         $response = $this->withHeaders([
             'Authorization' => 'Bearer ' . $this->token,
         ])->json('POST', route('peers.syndicate.post'), [
-            'post' => $post,
+            'post' => $this->post->prepareForSyndication('saved'),
             'event' => 'saved',
         ]);
 
@@ -78,6 +89,109 @@ class SyndicationTest extends TestCase
 
         Livewire::test(ListPosts::class)
             ->set('feed', true)
-            ->assertSee($text->title);
+            ->assertSee($this->post->postable->title);
+    }
+
+    /** @test */
+    public function will_delete_peer_posts()
+    {
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->token,
+        ])->json('POST', route('peers.syndicate.post'), [
+            'post' => $this->post->prepareForSyndication('saved'),
+            'event' => 'saved',
+        ]);
+
+        $response->assertStatus(200);
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->token,
+        ])->json('POST', route('peers.syndicate.post'), [
+            'post' => $this->post->prepareForSyndication('deleted'),
+            'event' => 'deleted',
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->actingAs($this->user);
+
+        $this->assertCount(0, Post::syndicated()->get());
+
+        Livewire::test(ListPosts::class)
+            ->set('feed', true)
+            ->assertDontSee($this->post->postable->title);
+    }
+
+    /** @test */
+    public function will_accept_peer_comments()
+    {
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->token,
+        ])->json('POST', route('peers.syndicate.post'), [
+            'post' => $this->post->prepareForSyndication('saved'),
+            'event' => 'saved',
+        ]);
+
+        $response->assertStatus(200);
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->token,
+        ])->json('POST', route('peers.syndicate.comment'), [
+            'post' => json_encode($this->post->only(['uuid'])),
+            'comment' =>  $this->comment->toJson(),
+            'event' => 'saved',
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->actingAs($this->user);
+
+        $this->assertCount(1, Post::syndicated()->get());
+
+        Livewire::test(ViewPost::class)
+            ->call('open', Post::syndicated()->get()->first()->slug)
+            ->assertSee($this->post->postable->title)
+            ->assertSee($this->comment->comment);
+    }
+
+    /** @test */
+    public function will_delete_peer_comments()
+    {
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->token,
+        ])->json('POST', route('peers.syndicate.post'), [
+            'post' => $this->post->prepareForSyndication('saved'),
+            'event' => 'saved',
+        ]);
+
+        $response->assertStatus(200);
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->token,
+        ])->json('POST', route('peers.syndicate.comment'), [
+            'post' => json_encode($this->post->only(['uuid'])),
+            'comment' =>  $this->comment->toJson(),
+            'event' => 'saved',
+        ]);
+
+        $response->assertStatus(200);
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->token,
+        ])->json('POST', route('peers.syndicate.comment'), [
+            'post' => json_encode($this->post->only(['uuid'])),
+            'comment' =>  json_encode($this->comment->only(['uuid'])),
+            'event' => 'deleted',
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->actingAs($this->user);
+
+        $this->assertCount(0, Comment::all());
+
+        Livewire::test(ViewPost::class)
+            ->call('open', Post::syndicated()->get()->first()->slug)
+            ->assertDontSee($this->comment->comment);
     }
 }
